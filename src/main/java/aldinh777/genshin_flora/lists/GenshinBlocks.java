ed6d48f13b1db0ca495599d047063449394aa8ea
aldinh777 <aldi.nh789@gmail.com>
2020-10-14 18:09:55 +0800

    Functioning Mist Flower

--- src/main/java/aldinh777/genshin_flora/lists/GenshinBlocks.java
+++ src/main/java/aldinh777/genshin_flora/lists/GenshinBlocks.java
@@ -1,6 +1,7 @@
 package aldinh777.genshin_flora.lists;
 
 import aldinh777.genshin_flora.flowers.FlamingFlower;
+import aldinh777.genshin_flora.flowers.MistFlower;
 import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 
@@ -11,8 +12,10 @@ public class GenshinBlocks {
     public static List<Block> LIST = Lists.newArrayList();
 
     public static Block FLAMING_FLOWER;
+    public static Block MIST_FLOWER;
 
     public static void init() {
         FLAMING_FLOWER = new FlamingFlower("flaming_flower");
+        MIST_FLOWER = new MistFlower("mist_flower");
     }
 }
